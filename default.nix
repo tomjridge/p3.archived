6ede2e71b24ec6b721395fdf7d293ee17e8f51a0
Tom Ridge <tom.j.ridge@googlemail.com>
2015-05-12 15:47:08 +0100

    +

--- default.nix
+++ default.nix
@@ -27,6 +27,33 @@ in rec {
 
   };
 
+  # following needs 4.01.0 because of string literal problem
+  p3_2014-04-30 = stdenv.mkDerivation {
+    name = "p3";
+  
+    src = fetchgit {
+      url = https://github.com/tomjridge/p3.git;
+      rev = "1ee8caf"; # 2014-04-30
+      sha256 = "6e78de60e45b1ab72d4892f24345a921e546774d4ed3a1578d1bac73494c5394";
+    };
+  
+    buildInputs = [ pkgs.ocaml_4_01_0 pkgs.ocamlPackages.findlib ]; # note use of 4.01.0
+  
+    configurePhase = "true"; 	# Skip configure
+
+    postBuild = '' 
+      mkdir -p $out
+      cp -R -L build examples $out # want symlinks to point in $out, not at their original targets
+    '';
+    
+    installPhase= "true"; # skip install  
+
+#    createFindlibDestdir = true;
+
+
+  };
+
+
   # get a nix-shell environment with p3 installed via ocamlfind
   post_install = stdenv.mkDerivation {
     name = "post_install";
